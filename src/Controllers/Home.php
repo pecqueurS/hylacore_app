9b100a20f7a8002f3d9310448c93ef1d6c8d7d89
Stephane <stephane.pecqueur@gmail.com>
2015-12-03 00:09:24 +0100

    test model db

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -5,7 +5,12 @@ namespace Example\Controllers;
 use Example\Models\Test;
 use Hyla\Controller\Controller;
 use Hyla\Db\Db;
+use Hyla\Orm\Unit;
 
+/**
+ * Class Home
+ * @package Example\Controllers
+ */
 class Home extends Controller {
     public function show($id, $name = null)
     {
@@ -13,7 +18,24 @@ class Home extends Controller {
         $select = $test->select();
         var_dump($select);
 
-        //$test->
+        $unit = new Unit(array('id', 'name', 'price', 'category'), true);
+        $unit->name = 'test name';
+        $unit->price = '32';
+        $unit->category = 'test category';
+        $test->saveRows(array($unit));
+
+        $select = $test->select();
+        var_dump($select);
+
+        $test->saveRows(array(array(
+            'id' => null,
+            'name' => 'test Name 2',
+            'price' => 'test price 2',
+            'category' => 'cat 2'
+        )));
+
+        $select = $test->select();
+        var_dump($select);
 
 
         $this->addResponse('id', $id);
--- src/Models/Test.php
+++ src/Models/Test.php
@@ -3,6 +3,10 @@ namespace Example\Models;
 
 use Hyla\Orm\Cluster;
 
+/**
+ * Class Test
+ * @package Example\Models
+ */
 class Test extends Cluster {
     protected $type = self::DB;
     protected $table = 'test';
